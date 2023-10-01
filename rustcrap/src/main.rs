17831e82e3769d4a37134da2d0975f92b67baf32
BarbarianTarkus <richvclaros@gmail.com>
2023-10-01 07:39:51 +0200

    scrapper: improved csv writing

---
+++ rustcrap/src/main.rs
@@ -0,0 +1,89 @@
+use reqwest::blocking::{get, Client};
+use scraper::{self, html, Selector};
+use select::document::Document;
+use select::predicate::Name;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
+use csv::{Writer, WriterBuilder};
+
+#[derive(Debug)]
+#[derive(serde::Serialize)]
+struct Item {
+    title: Option<String>,
+    image: Option<String>,
+    url: Option<String>,
+    description: Option<String>,
+    magnet: Option<String>,
+    size: Option<String>,
+}
+
+impl Item {
+    fn new(html_item: scraper::ElementRef) -> Self {
+        let title = html_item
+            .select(&Selector::parse(".entry-title").unwrap())
+            .next()
+            .map(|title| title.text().collect::<String>());
+
+        let image = html_item
+            .select(&Selector::parse("a > img").unwrap())
+            .next()
+            .and_then(|img| img.value().attr("src"))
+            .map(str::to_owned);
+
+        let url = html_item
+            .select(&Selector::parse(".entry-title a").unwrap())
+            .next()
+            .and_then(|a| a.value().attr("href"))
+            .map(str::to_owned);
+
+        let description = None;
+
+        let magnet = html_item
+            .select(&Selector::parse("a[href^=\"magnet\"]").unwrap())
+            .next()
+            .and_then(|a| a.value().attr("href"))
+            .map(str::to_owned);
+
+        let size = html_item
+            .select(&Selector::parse("div > p:nth-child(2) > strong:nth-child(11)").unwrap())
+            .next()
+            .map(|size| size.text().collect::<String>());
+
+        Item {
+            title,
+            image,
+            url,
+            description,
+            magnet,
+            size,
+        }
+    }
+}
+
+fn output_items_to_csv(items: Vec<Item>) {
+    let mut writer = WriterBuilder::new()
+        .has_headers(true)
+        .from_writer(File::create("output.csv").unwrap());
+
+
+    for item in items {
+        if let Some(_) = item.magnet {
+            writer.serialize(item).unwrap();
+        }
+    }
+}
+
+fn main() {
+    let response = get("https://fitgirl-repacks.site/").unwrap();
+    let data: String = response.text().unwrap();
+    let document = scraper::Html::parse_document(&data);
+
+    let html_item_selector = Selector::parse("article.post").unwrap();
+    let html_items = document.select(&html_item_selector);
+
+    let items: Vec<Item> = html_items.map(Item::new).collect();
+
+    // output it to csv
+    output_items_to_csv(items);
+}
